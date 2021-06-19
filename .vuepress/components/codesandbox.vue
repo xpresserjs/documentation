3d3dd27ee54d406204e608aad24180c7937e2501
trapcodeio <trapcode.io@gmail.com>
2021-06-19 09:36:42 +0100

    Updated Xpresser Docs

--- .vuepress/components/codesandbox.vue
+++ .vuepress/components/codesandbox.vue
@@ -12,7 +12,7 @@
     <template v-else>
       <iframe src="https://codesandbox.io/embed/github/xpresserjs/new-app/tree/master/?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
               style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-              title="new-app-ts"
+              title="new-app"
               allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
               sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
       ></iframe>
